0b04c7b4e2d85b6b6b164df45627bfbd1eeaec4a
miltondavalos <miltondavalos@gmail.com>
2014-07-08 23:59:45 -0400

    lesson 2

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comments_params)
+    @comment.creator = User.first
+
+
+    if @comment.save
+      flash[:notice] = 'Your comment was added successfully!'
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'  ,
+    end
+  end
+
+  private
+  def comments_params
+    params.require(:comment).permit(:body)
+  end
+end
