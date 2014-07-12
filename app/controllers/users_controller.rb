b4dab2e79e0482d33d2d83014bb209df7957e9c1
miltondavalos <miltondavalos@gmail.com>
2014-07-12 14:05:23 -0400

    after solution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit!)
+    @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = 'You have register successfully!'
       redirect_to root_path
     else
+      #flash[:error] = 'Invalid username/password'   move to template
       render :new
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
