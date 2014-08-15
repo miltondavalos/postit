1824a13d77375c3fb29ab8a6c592b25c048a08b6
miltondavalos <miltondavalos@gmail.com>
2014-08-14 22:47:43 -0400

    better slugs

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,10 +3,36 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :post_categories
 
   validates :name, presence: true, uniqueness: true, length: {minimum: 2}
-  before_save :generate_slug
+  before_save :generate_slug!
 
-  def generate_slug
-    self.slug = self.name.gsub(' ','-').downcase
+  #def generate_slug
+  #  self.slug = self.name.gsub(' ','-').downcase
+  #end
+  def generate_slug!
+    the_slug = to_slug(self.title)
+    category = Post.find_by slug: the_slug
+    count = 2
+    while category && category != self
+      the_slug = append_suffix(the_slug, count)
+      category = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
   end
 
   def to_param
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,38 @@ class User < ActiveRecord::Base
 
   has_secure_password validations: false
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, on: :create, length:{minimum: 5}
+  validates :password, presence: true, on: :create, length: {minimum: 5}
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
-  def generate_slug
-    self.slug = self.username.gsub(' ','-').downcase
+  #def generate_slug
+  #  self.slug = self.username.gsub(' ','-').downcase
+  #end
+  def generate_slug!
+    the_slug = to_slug(self.title)
+    user = Post.find_by slug: the_slug
+    count = 2
+    while user && user != self
+      the_slug = append_suffix(the_slug, count)
+      user = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
   end
 
   def to_param
