7bdd3f0e77e9eb450ed06a9df4ac2fde1264946b
miltondavalos <miltondavalos@gmail.com>
2014-08-17 20:22:42 -0400

    add module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
+  include Voteable
   #belongs_to :user
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
+  #has_many :votes, as: :voteable
 
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true
@@ -12,17 +13,17 @@ class Post < ActiveRecord::Base
 
   before_save :generate_slug!
 
-  def total_votes
-    up_votes - down_votes
-  end
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
+  #def total_votes
+  #  up_votes - down_votes
+  #end
+  #
+  #def up_votes
+  #  self.votes.where(vote: true).size
+  #end
+  #
+  #def down_votes
+  #  self.votes.where(vote: false).size
+  #end
 
   #def generate_slug
   #  self.slug = self.title.gsub(' ','-').downcase
